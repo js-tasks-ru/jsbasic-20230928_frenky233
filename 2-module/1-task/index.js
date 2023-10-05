8566a2205f49e2af3c2f3b7d9d12bd5683579401
Frenky233 <supertiger46590@gmail.com>
2023-10-05 23:03:19 +0500

    sumSalary task

--- 2-module/1-task/index.js
+++ 2-module/1-task/index.js
@@ -1,3 +1,9 @@
 function sumSalary(salaries) {
-  // ваш код...
+  let val = 0;
+  for(let key in salaries){
+    if(typeof salaries[key] == 'number' && !isNaN(salaries[key]) && isFinite(salaries[key])){ 
+      val += salaries[key];
+    }
+  }
+  return val;
 }
