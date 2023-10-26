1c06311cfa1e09612df27e1cf402b5306c79dd2c
Frenky233 <supertiger46590@gmail.com>
2023-10-27 01:31:00 +0500

    UserTable task

--- 6-module/1-task/index.js
+++ 6-module/1-task/index.js
@@ -12,7 +12,44 @@
  *      }
  *
  */
+
 export default class UserTable {
   constructor(rows) {
+    this.rows = rows;
+    this.render();
+  }
+  
+  render(){
+    this.elem = document.createElement('TABLE');
+
+    this.elem.insertAdjacentHTML('beforeend',
+    `<thead>
+        <tr>
+          <th>Имя</th>
+          <th>Возраст</th>
+          <th>Зарплата</th>
+          <th>Город</th>
+          <th></th>
+        </tr>
+      </thead>`
+    );
+    
+    this.elem.insertAdjacentHTML('beforeend',
+    this.rows.map((item) =>
+    `<tr>
+    ${Object.values(item).map((item) => 
+      `<td>${item}</td>`).join('')}
+    <td><button>X</button></td>
+    </tr>`).join('')
+    );
+
+    this.elem.addEventListener('click', this.deleteTr);
+  }
+
+  deleteTr(event){
+    const button = event.target.closest('button');
+    if(button){
+      event.target.closest('TR').remove();
+    }
   }
 }
