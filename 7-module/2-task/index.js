53bf45d8c825079ddf9fe2a5fb2f0e8737080716
Frenky233 <supertiger46590@gmail.com>
2023-10-31 12:49:39 +0500

    Modal task

--- 7-module/2-task/index.js
+++ 7-module/2-task/index.js
@@ -2,5 +2,60 @@ import createElement from '../../assets/lib/create-element.js';
 
 export default class Modal {
   constructor() {
+    this.render();
+  }
+  
+  render() {
+    this.elem = createElement(
+      `<div class="modal">
+        <div class="modal__overlay"></div>
+          <div class="modal__inner">
+            <div class="modal__header">
+              <button type="button" class="modal__close">
+                <img src="/assets/images/icons/cross-icon.svg" alt="close-icon" />
+              </button>
+              <h3 class="modal__title">
+              </h3>
+            </div>
+            <div class="modal__body">
+            </div>
+          </div>  
+      </div>`
+    );
+  }
+
+  open(){
+    document.body.append(this.elem);
+    document.body.classList.add('is-modal-open');
+
+    this.elem.addEventListener('click', this.onClick);
+    document.body.addEventListener('keydown', this.escPressed)
+  }
+
+  setTitle(title) {
+    this.elem.querySelector('.modal__title').innerHTML = title;
+  }
+
+  setBody(node) {
+    this.elem.querySelector('.modal__body').innerHTML = node.outerHTML;
+  }
+
+  onClick = (event) => {
+    const button = event.target.closest('button');
+    if(button){
+      this.close();
+    }
+  }
+
+  escPressed = (event) => {
+    if(event.code === 'Escape'){
+      document.body.removeEventListener('keydown', this.escPressed)
+      this.close();
+    }
+  }
+
+  close(){
+    this.elem.remove();
+    document.body.classList.remove('is-modal-open')
   }
 }
